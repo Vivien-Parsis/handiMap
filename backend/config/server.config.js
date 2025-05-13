c394af2da4adcfb7053d932ab9dcde57d3d7a9e1
Vivien-Parsis <94974717+Vivien-Parsis@users.noreply.github.com>
2025-05-13 09:48:19 +0200

    Update server.config.js

--- backend/config/server.config.js
+++ backend/config/server.config.js
@@ -2,7 +2,7 @@ const dotenv = require("dotenv")
 dotenv.config()
 
 const db_url = process.env.DB_URL || ""
-const host = process.env.HOST || "localhost"
+const host = ("RENDER" in process.env) ? `0.0.0.0` : `localhost`
 const port = process.env.PORT || 4000
 const jwt_secret = process.env.JWT_SECRET || "JWT"
 const frontend_url = process.env.FRONT_URL || "http://localhost:3000"
