8efa2eafbc6fdf069160f27f6a02cd16ed2d08d8
Vivien-Parsis <vivienparsis.perso@gmail.com>
2025-05-20 09:11:07 +0200

    update upload middleware

--- backend/middlewares/upload.middleware.js
+++ backend/middlewares/upload.middleware.js
@@ -1,6 +1,11 @@
 import { storage } from "../config/cloudinary.config.js"
 import multer from "multer"
 
-const upload = multer({ storage: storage })
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 5 * 1024 * 1024
+    }
+})
 
 export { upload }
