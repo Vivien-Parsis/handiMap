12485853f2380112bba1b0f7162fdec797328b65
Vivien-Parsis <94974717+Vivien-Parsis@users.noreply.github.com>
2025-05-26 18:13:09 +0200

    Update auth.route.js

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,9 +1,10 @@
 import { login, register } from "../controllers/auth.controller.js"
 import { Router } from "express"
+import { rateLimit } from "../middlewares/rateLimiter.middleware.js"
 const authRouter = Router()
 
-authRouter.post("/login", login)
-authRouter.post("/register", register)
+authRouter.post("/login", login, rateLimit)
+authRouter.post("/register", register, rateLimit)
 
 export{
     authRouter
