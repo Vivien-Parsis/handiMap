3c2022e0235aeb47db2dfab3710dd832a548f476
Vivien-Parsis <94974717+Vivien-Parsis@users.noreply.github.com>
2025-06-02 19:27:29 +0200

    Update handicap.route.js

--- backend/routes/handicap.route.js
+++ backend/routes/handicap.route.js
@@ -2,6 +2,38 @@ import { getAllHandicaps } from "../controllers/handicap.controller.js"
 import { Router } from "express"
 const handicapRouter = Router()
 
+/**
+ * @swagger
+ * tags:
+ *   name: Handicap
+ *   description: Opérations liées aux types de handicaps
+ */
+
+/**
+ * @swagger
+ * /handicaps:
+ *   get:
+ *     summary: Récupérer tous les types de handicaps
+ *     tags: [Handicap]
+ *     responses:
+ *       200:
+ *         description: Liste des handicaps
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id_handicap:
+ *                     type: integer
+ *                   nom_handicap:
+ *                     type: string
+ *                   type_handicap:
+ *                     type: string
+ *       500:
+ *         description: Erreur serveur
+ */
 handicapRouter.get("/", getAllHandicaps)
 
 export {
