d8537bf266ba963a66c83a9f1910c274bc7f4af5
Vivien-Parsis <94974717+Vivien-Parsis@users.noreply.github.com>
2025-06-16 13:24:34 +0200

    Update logger.middleware.js

--- backend/middlewares/logger.middleware.js
+++ backend/middlewares/logger.middleware.js
@@ -5,9 +5,11 @@ import fs from 'fs'
 if (!fs.existsSync('../log')) {
     fs.mkdirSync('../log', { recursive: true })
 }
-
-const accessLogStream = fs.createWriteStream('../log/access.log', { flags: 'a' })
-const DevLogConsole = 'HTTP :http-version || status code :status || method :method || :date[web] || response time :response-time ms || url :url || :user-agent'
+morgan.token("body", (req, res) => {
+    return JSON.stringify(req.body)   
+})
+const accessLogStream = fs.createWriteStream(`${(node_env == "DEV" || node_env == "TEST") ? '../log/dev.log' : (node_env == "PROD") ? '../log/access.log' : '../log/log.log'}`, { flags: 'a' })
+const DevLogConsole = 'HTTP :http-version || status code :status || method :method || :date[web] || response time :response-time ms || url :url || :body || :user-agent'
 const DevLogWrite = ':http-version;:status;:method;:date[web];:response-time ms;:url;:user-agent'
 
 const ProdLogConsole = 'HTTP :http-version || status code :status || method :method || :date[web] || response time :response-time ms || url :url'
