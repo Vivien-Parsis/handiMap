91c59cff1d155ae69ec9fd869d3713b68c29140d
Vivien-Parsis <94974717+Vivien-Parsis@users.noreply.github.com>
2025-06-19 14:28:35 +0200

    update map

--- front-end/src/pages/map/map.jsx
+++ front-end/src/pages/map/map.jsx
@@ -108,7 +108,7 @@ const Maps = () => {
       >
         <TileLayer
           attribution='&copy; <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>'
-          url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+          url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}"
         />
         <ZoomControl position="bottomleft" />
         {forMarker(etablisements)}
